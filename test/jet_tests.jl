fc3b6c3dd921a1bce9a2aeabf1168e60c102916a
Marius Fersigan <marius.fersigan@gmail.com>
2026-02-15 18:25:26 +0200

    fix: Remove JET dependency and update test execution for compatibility with Julia 1.12

--- test/jet_tests.jl
+++ test/jet_tests.jl
@@ -1,23 +1,9 @@
 using JET
 using Neo4jQuery
-using Pkg
 using Test
 
-function get_pkg_version(name::AbstractString)
-    deps = Pkg.dependencies()
-    for (_, info) in deps
-        if info.name == name
-            return info.version
-        end
-    end
-    return nothing
-end
-
 @testset "Type Stability (JET.jl)" begin
-    if VERSION >= v"1.12"
-        @assert get_pkg_version("JET") >= v"0.11"
-        JET.test_package(Neo4jQuery;
-            target_modules=(Neo4jQuery,),
-            ignore_missing_comparison=true)
-    end
+    JET.test_package(Neo4jQuery;
+        target_modules=(Neo4jQuery,),
+        ignore_missing_comparison=true)
 end
